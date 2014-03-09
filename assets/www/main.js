cd1a747a6cbb191fffabef19fb7aa576aeccd068
seth <seth@beamdog.com>
2014-03-09 16:48:07 -0600

    added milestone functionality and removed old ajax prototype

--- assets/www/main.js
+++ assets/www/main.js
@@ -21,6 +21,10 @@
 		{
 			getClosedIssues();
 		}
+		else if(window.location == "file:///android_asset/www/display.html" && localStorage['whatToLoad'] == "Milestones")
+		{
+			getMilestoneDates();
+		}
 		else
 		{
 			alert(window.location + " : " + localStorage['whatToLoad']);
@@ -372,10 +376,88 @@
 		}
 	}
 	
+	function loadMilestoneDates()
+	{
+		localStorage['whatToLoad'] = "Milestones";
+		window.location = "display.html";
+	}
+	
 	function getMilestoneDates()
 	{
-		localStorage['whatToLoad'] = "MyIssues";
-		window.location = "display.html";
+		$.ajax
+	    (
+	    	{
+        		type: "GET",
+        		async: false,
+        		url: "https://api.github.com/repos/" + localStorage['ownername'] + "/" + localStorage['repository'] + "/milestones",
+        		dataType: "json",
+        		success: function(result) 
+        		{	
+            		for( i in result ) 
+            		{
+                		<!-- Link to issue hidden under milestone name -->
+                		$("#issue_list").append
+                		(
+                    		"<li><a href='" + result[i].url + "' target='_blank'>" +
+                    		result[i].title + "</a></li>"
+                		);
+                
+		                <!-- Author name -->
+		                $("#issue_list").append
+		                (
+		                    "Author: " + result[i].creator.login + "<br>"
+		                );
+                
+		                <!-- Milestone body -->
+		                $("#issue_list").append
+		                (
+		                    "Details: " + result[i].description + "<br>"
+		                );
+                
+		                <!-- Milestone state -->
+		                $("#issue_list").append
+		                (
+		                    "Open/Closed: " + result[i].state + "<br>"
+		                );
+		                
+		                <!-- Date created -->
+		                $("#issue_list").append
+		                (
+		                    "Date created: " + result[i].created_at + "<br>"
+		                );
+		                
+		                <!-- Date updated -->
+		                $("#issue_list").append
+		                (
+		                    "Date updated: " + result[i].updated_at + "<br>"
+		                );
+		                
+		                <!-- Date Closed -->
+		                $("#issue_list").append
+		                (
+		                    "Date Closed: " + result[i].closed_at + "<br>"
+		                );
+		                
+		                <!-- Date Due -->
+		                $("#issue_list").append
+		                (
+		                    "Date Closed: " + result[i].due_on + "<br>"
+		                );
+		                
+		                <!-- Issues -->
+		                $("#issue_list").append
+		                (
+		                    "Progress: " + 100*(result[i].closed_issues / (result[i].closed_issues + result[i].open_issues) )
+		                    + "% ( " + result[i].open_issues+ " open : "
+		                    + result[i].closed_issues + " closed ) "
+		                    + "<br>"
+		                );
+    
+                		console.log("i: " + i);
+            		}
+	        	}
+	    	}
+		)
 	}
 	
 	function clearData()
@@ -457,174 +539,4 @@
         )
         
         return returnData;
-	}
-
-	function getData() 
-	{
-		msg();
-		var text = "WORKING";
-		var totalIssues = 0;
-	    $.ajax
-	    (
-	    	{
-        		type: "GET",
-        		async: false,
-        		url: "https://api.github.com/repos/" + localStorage['ownername'] + "/" + localStorage['repository'] + "/issues",
-        		dataType: "json",
-        		success: function(result) 
-        		{	
-        			totalIssues += result.length;
-            		for( i in result ) 
-            		{
-                		<!-- Link to issue hidden under issue name -->
-                		$("#issue_list").append
-                		(
-                    		"<li><a href='" + result[i].url + "' target='_blank'>" +
-                    		result[i].title + "</a></li>"
-                		);
-                
-                		text = text + "Author: " + result[i].user.login + "\n";
-		                <!-- Author name -->
-		                $("#issue_list").append
-		                (
-		                    "Author: " + result[i].user.login + "<br>"
-		                );
-                
-		                <!-- Issue body -->
-		                $("#issue_list").append
-		                (
-		                    "Details: " + result[i].body + "<br>"
-		                );
-                
-		                <!-- Issue state -->
-		                $("#issue_list").append
-		                (
-		                    "Open/Closed: " + result[i].state + "<br>"
-		                );
-		                
-		                <!-- Issue assignee -->
-		                $("#issue_list").append
-		                (
-		                    "User assigned: " + result[i].assignee.login + "<br>"
-		                );
-		                
-		                <!-- Milestone -->
-		                $("#issue_list").append
-		                (
-		                    "Milestone: " + result[i].milestone + "<br>"
-		                );
-		                
-		                <!-- Comments -->
-		                $("#issue_list").append
-		                (
-		                    "Number of comments: " + result[i].comments + "<br>"
-		                );
-		                
-		                <!-- Date created -->
-		                $("#issue_list").append
-		                (
-		                    "Date created: " + result[i].created_at + "<br>"
-		                );
-		                
-		                <!-- Date updated -->
-		                $("#issue_list").append
-		                (
-		                    "Date updated: " + result[i].updated_at + "<br>"
-		                );
-		                
-		                <!-- Date Closed -->
-		                $("#issue_list").append
-		                (
-		                    "Date Closed: " + result[i].closed_at + "<br>"
-		                );
-    
-                		console.log("i: " + i);
-            		}
-		            console.log(result);
-	        	}
-	    	}
-		);
-		$.ajax
-	    (
-	    	{
-        		type: "GET",
-        		async: false,
-        		url: "https://api.github.com/repos/" + localStorage['username'] + "/" + localStorage['repository'] + "/issues?state=closed",
-        		dataType: "json",
-        		success: function(result) 
-        		{	
-        			totalIssues += result.length;
-            		for( i in result ) 
-            		{
-                		<!-- Link to issue hidden under issue name -->
-                		$("#issue_list").append
-                		(
-                    		"<li><a href='" + result[i].url + "' target='_blank'>" +
-                    		result[i].title + "</a></li>"
-                		);
-                
-                		text = text + "Author: " + result[i].user.login + "\n";
-		                <!-- Author name -->
-		                $("#issue_list").append
-		                (
-		                    "Author: " + result[i].user.login + "<br>"
-		                );
-                
-		                <!-- Issue body -->
-		                $("#issue_list").append
-		                (
-		                    "Details: " + result[i].body + "<br>"
-		                );
-                
-		                <!-- Issue state -->
-		                $("#issue_list").append
-		                (
-		                    "Open/Closed: " + result[i].state + "<br>"
-		                );
-		                
-		                <!-- Issue assignee -->
-		                $("#issue_list").append
-		                (
-		                    "User assigned: " + result[i].assignee.login + "<br>"
-		                );
-		                
-		                <!-- Milestone -->
-		                $("#issue_list").append
-		                (
-		                    "Milestone: " + result[i].milestone + "<br>"
-		                );
-		                
-		                <!-- Comments -->
-		                $("#issue_list").append
-		                (
-		                    "Number of comments: " + result[i].comments + "<br>"
-		                );
-		                
-		                <!-- Date created -->
-		                $("#issue_list").append
-		                (
-		                    "Date created: " + result[i].created_at + "<br>"
-		                );
-		                
-		                <!-- Date updated -->
-		                $("#issue_list").append
-		                (
-		                    "Date updated: " + result[i].updated_at + "<br>"
-		                );
-		                
-		                <!-- Date Closed -->
-		                $("#issue_list").append
-		                (
-		                    "Date Closed: " + result[i].closed_at + "<br>"
-		                );
-    
-                		console.log("i: " + i);
-            		}
-
-		            console.log(result);
-		            document.getElementById("issue_count").innerHTML = "";
-					$("#issue_count").append("Total Issues: " + totalIssues);
-	        	}
-	    	}
-		);
 	}
