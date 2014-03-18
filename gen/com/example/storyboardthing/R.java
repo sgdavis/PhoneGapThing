a3ac1d5ff92b821fde1cfbf6f3223bd8619f01f2
seth <seth@beamdog.com>
2014-03-17 23:34:36 -0600

    even overviews - problem with payloads

--- gen/com/example/storyboardthing/R.java
+++ gen/com/example/storyboardthing/R.java
@@ -24,12 +24,15 @@ public final class R {
         public static final int splash=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090002;
+        public static final int action_settings=0x7f090004;
+        public static final int atomPay_removePay=0x7f090003;
+        public static final int listview1=0x7f090002;
         public static final int notificationButton=0x7f090001;
         public static final int textView1=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int listbutton=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f080000;
--- src/com/example/storyboardthing/MainActivity.java
+++ src/com/example/storyboardthing/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.storyboardthing;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -9,7 +10,9 @@ import java.util.Map;
 
 import org.apache.cordova.DroidGap;
 import org.apache.cordova.api.LOG;
+import org.kohsuke.github.GHEvent;
 import org.kohsuke.github.GHEventInfo;
+import org.kohsuke.github.GHEventPayload;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.PagedIterable;
@@ -20,13 +23,18 @@ import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.storyboardthing.R;
 
@@ -34,10 +42,18 @@ public class MainActivity extends DroidGap
 {
 	private long lastChecked;
 	public TextView textview;
+	private Context myContext;
+	
+	private String repoName = "PhoneGapThing";
+	private String ownerName = "sgdavis";
+	
+	//private String repoName = "example-project";
+	//private String ownerName = "ajyong";
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) 
 	{
+		myContext = this;
 		super.onCreate(savedInstanceState);	
 		super.init();
 		super.loadUrl("file:///android_asset/www/index.html");
@@ -81,7 +97,7 @@ public class MainActivity extends DroidGap
 			}
 		);
 		
-		lastChecked = System.currentTimeMillis();
+		lastChecked = System.currentTimeMillis();	
 	}
 	
 	public void startChecking()
@@ -161,9 +177,142 @@ public class MainActivity extends DroidGap
     		textview.setText( tempString );
     	}
     }
+    /*
+    COMMIT_COMMENT 
+    CREATE 
+    DELETE 
+    DOWNLOAD 
+    FOLLOW 
+    FORK 
+    FORK_APPLY 
+    GIST 
+    GOLLUM 
+    ISSUE_COMMENT 
+    ISSUES 
+    MEMBER 
+    PUBLIC 
+    PULL_REQUEST 
+    PULL_REQUEST_REVIEW_COMMENT 
+    PUSH 
+    TEAM_ADD 
+    WATCH 
+    */
+    public String getEventOverview(GHEventInfo event)
+    {
+    	String ret = "";
+    	GHEventPayload temp;
+    	GHEventPayload.IssueComment issueComment = new GHEventPayload.IssueComment();
+    	GHEventPayload.PullRequest pullRequest = new GHEventPayload.PullRequest();
+    	GHEventPayload.Push push = new GHEventPayload.Push();
+    	String supportString = "";
+    	try
+    	{
+    		switch(event.getType())
+    		{
+    		case ISSUE_COMMENT:
+    			//issueComment = event.getPayload(issueComment.getClass());
+				ret = "Comments on issue *" + "" + "* by user *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+    		case PULL_REQUEST:
+    			//pullRequest = event.getPayload(pullRequest.getClass());
+    			ret = "Data pulled by user *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+    		case PUSH:
+    			//push = event.getPayload(push.getClass());
+    			ret = "Data pushed by user *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+    		case COMMIT_COMMENT:
+    			ret = "Comments on commit *" + "" + "* by user *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+    		case CREATE:
+    			//payload ref_type Can be one of “repository”, “branch”, or “tag”
+    			if(false)
+    			{
+    				supportString = "Branch *" + "" + "*";
+    			}
+    			if(false)
+    			{
+    				supportString = "Tag *" + "" + "*";
+    			}
+    			else
+    			{
+    				supportString = "Repository";
+    			}
+    			ret = supportString + " created by user *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+    		case DELETE:
+    			if(false)
+    			{
+    				supportString = "Branch *" + "" + "*";
+    			}
+    			if(false)
+    			{
+    				supportString = "Tag *" + "" + "*";
+    			}
+    			else
+    			{
+    				supportString = "Repository";
+    			}
+    			//payload ref_type Can be one of “branch”, or “tag”
+    			ret = supportString + " destroyed by user *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+    		case DOWNLOAD:
+    			//deprecated by github
+    			ret = "Repository downloaded by user *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+    		case FOLLOW:
+    			//only user to user
+    			ret = "User followed by user *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+    		case FORK:
+    			ret = "Repository forked by user *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+    		case FORK_APPLY:
+    			//deprecated by github
+    			ret = "Fork application by user *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+    		case GIST:
+    			//deprecated by github
+    			ret = "GIST created by user *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+    		case GOLLUM:
+    			ret = "Wiki page *" + "" + "* updated by user *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+    		case ISSUES:
+    			//payload action Can be one of “opened”, “closed”, or “reopened”.
+    			ret = "Issue *" + "" + "* created by user *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+    		case MEMBER:
+    			ret = "Collaborator *" + "" + "* added by *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+    		case PUBLIC:
+    			ret = "Repository made public by *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+    		case PULL_REQUEST_REVIEW_COMMENT:
+    			ret = "Repository publicity changed by *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+    		case TEAM_ADD:
+    			//user or repo added to a team
+				break;
+    		case WATCH:
+    			ret = "Repository starred by *" + event.getActorLogin() + "* on " + event.getCreatedAt();
+				break;
+			default:
+				ret = "Unhandled Event";
+				break;
+    		}
+    	}
+    	catch (IOException e)
+    	{
+				LOG.i("WAFFLE", "cannot summarize");
+    	}
+    	
+    	return ret;
+    }
     
     class AsyncTaskRunner extends AsyncTask<String, String, String> 
     {
+    	List<GHEventInfo> eventList;
   	  	private String resp;
 
   	  	@Override
@@ -173,9 +322,9 @@ public class MainActivity extends DroidGap
   			{
 	  	      	GitHub github = GitHub.connectUsingPassword("sgdavis", "Z4ngetsuSeth");
 	  	      	LOG.i("WAFFLE", "connected");
-	  	      	GHRepository repository = github.getRepository("ajyong/example-project");
+	  	      	GHRepository repository = github.getRepository(ownerName + "/" + repoName);
 	  	      	LOG.i("WAFFLE", "repod");
-	  	      	List<GHEventInfo> eventList = repository.listEvents().asList();
+	  	      	eventList = repository.listEvents().asList();
 	  	      	LOG.i("WAFFLE", "evented");
 	  	      	
 	  	      	String tempString = "";
@@ -185,7 +334,7 @@ public class MainActivity extends DroidGap
 	  	      		for(int i = 0 ; i < eventList.size(); i++)
 	  	      		{
 	  	      			GHEventInfo tempEventInfo = eventList.get(i);
-	  	      			tempString = tempString + tempEventInfo.toString() + "\n";
+	  	      			tempString = tempString + getEventOverview(tempEventInfo) + "\n";
 	  	      		}
 	  	      		
 	  	      		LOG.i("WAFFLE", tempString );
@@ -195,7 +344,7 @@ public class MainActivity extends DroidGap
   			}
   			catch(IOException e)
   			{
-  				//textview.setText( "exception" );
+  				LOG.i("WAFFLE", "FAILED");
   			}
   	  		return "";
   	  	}
@@ -208,7 +357,27 @@ public class MainActivity extends DroidGap
 	  	  		{
 	  	  			public void run() 
 	  	  			{
-	  	  				textview.setText(strValue);   
+	  	  				//textview.setText(strValue);  
+		  	  			final ListView listview = (ListView) findViewById(R.id.listview1);		
+			  	  	    final ArrayList<String> list = new ArrayList<String>();
+			  	  	    for (int i = 0; i < eventList.size(); ++i) {
+			  	  	      list.add( getEventOverview(eventList.get(i)) );
+			  	  	    }
+			  	  	    final ArrayAdapter adapter = new ArrayAdapter(myContext, android.R.layout.simple_list_item_1, list);
+			  	  	    listview.setAdapter(adapter);
+		
+			  	  	    listview.setOnItemClickListener
+			  	  	    (
+			  	  	    	new AdapterView.OnItemClickListener() 
+			  	  		    {
+			  	  		      @Override
+			  	  		      public void onItemClick(AdapterView<?> parent, final View view, int position, long id) 
+			  	  		      {
+			  	  		    	  final String item = (String) parent.getItemAtPosition(position);
+			  	  		    	  Toast.makeText(getBaseContext(), item, Toast.LENGTH_LONG).show();
+			  	  		      }
+			  	  		    }
+			  	  	    );
 	  	  			}
 	  	  		}
   	  		);
